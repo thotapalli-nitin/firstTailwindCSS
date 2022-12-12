1c661ccec1122b1107a93385debdb4fcb35129dc
thotapalli-nitin <thotapalli.nitin482@gmail.com>
2022-12-13 00:23:46 +0530

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-1/3 h-16 bg-red-500 text-red-100 flex items-center justify-center text-xl">
+        Hello world!
+      </div>
+    </div>
+  );
+};
+
+export default App;
